267660d83041ef2271eca4748aef35a4bb79cdf3
sdotripley <scripley@akamai.com>
2015-11-10 14:18:28 -0500

    Abstracted and working
    Moved config data to file
    Abstracted pathnames

--- swipoll.py
+++ swipoll.py
@@ -7,6 +7,8 @@
 #
 #
 import os
+import re
+import inspect
 import pwd
 import sys
 import subprocess
@@ -21,41 +23,120 @@ import paramiko
 #from lxml import etree
 from io import StringIO, BytesIO
 
-#This is strictly for proof-of-concept when I forget how to xml things
-xmldoc = minidom.parse('/Users/scripley/xython2.7/xython2.7/item.xml')
-itemlist = xmldoc.getElementsByTagName('item')
-#print "Len : ", len(itemlist)
-#print "Attribute Name : ", itemlist[0].attributes['name'].value
-#print "Text : ", itemlist[0].firstChild.nodeValue
-#for s in itemlist :
-#    print "Attribute Name : ", s.attributes['name'].value
-#    print "Text : ", s.firstChild.nodeValue
-#
-#
-#
-#print "Len: ", len(itemlist)
-#print "Attribute: ", itemlist[0].nodeValue
-#print "Text: ",itemlist[0].firstChild.nodeValue
-#for s in itemlist :
-## print "Text: ", s.firstChild.nodeValue
-#    print "Value: ", s[0]
-#exit(0)
+# Files & Paths
+dirpath = os.getcwd()
+xmlpath = dirpath + "/item.xml"
+configfile = dirpath + "/config.data"
+xml_file = dirpath + "/show_int.xml"
+xml_out = dirpath + "/out.xml"
 
-switch = "SWITCH"
+# XML Formats
 
-xml_file = "/Users/scripley/xython2.7/xython2.7/show_int.xml"
-outfile = open('/Users/scripley/xython2.7/xython2.7/out.xml', 'wb')
-xmlout = subprocess.Popen(['cat', xml_file ], stdout=subprocess.PIPE)
-sshout = subprocess.Popen(['ssh', '-2A', switch, '-s', 'xmlagent'], stdin=xmlout.stdout, stdout=outfile)
-outfile.close()
+hello = """<?xml version="1.0" encoding="ISO-8859-1"?>
+    <hello xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+    <capabilities>
+    <capability>urn:ietf:params:xml:ns:netconf:base:1.0</capability>
+    <capability>urn:ietf:params:netconf:base:1.0</capability>
+    </capabilities>
+    <session-id>9605</session-id>
+    </hello>
+    ]]>]]>"""
 
-infile = open('/Users/scripley/out.xml','r')
-xmldump = infile.read()
-xmltodict.parse(xmldump, item_depth=3)
+xml_hed = """<?xml version="1.0" encoding="ISO-8859-1"?>"""
+hello_hed = "<hello"
+hello_end = "]]>]]>"
+
+
+def pollit(switch, xml_file, outfile):
+    
+    print xml_file, " & ", outfile
+    xmldest = open(outfile, 'wb')
+    xmlout = subprocess.Popen(['cat', xml_file ], stdout=subprocess.PIPE)
+    sshout = subprocess.Popen(['ssh', '-2A', switch, '-s', 'xmlagent'], stdin=xmlout.stdout, stdout=xmldest)
+    sshout.wait()
+    xmldest.close()
+
+
+# Main
+
+inspect.getfile(os)
+
+# Abstract out config data here
+with open(configfile,'rU') as configdata:
+    for line in configdata:
+        line = line.rstrip()
+        a = line.split(",")
+        
+        if a[0] == "SWITCH":
+            print "Switch = ", a[1]
+            switch = a[1]
+        elif a[0] == "ACCOUNT":
+            print "Account = ", a[1]
+            account = a[1]
+        elif a[0] == "PASSWORD":
+            print "Password = ", a[1]
+            password = a[1]
+
+pollit(switch, xml_file, xml_out)
+
+print("\n################################\n\n\n")
+xsd_src = open('netconf.xsd','rb')
+
+with open(xml_out, 'r') as file:
+    xml_data = file.read()
+
+#print xml_data
+xml_lines = xml_data.split('\n')
+
+for i, line in enumerate(xml_lines, start=0):
+    
+    if hello_end in line:
+        break
+    else:
+        continue
+
+clean_xml = []
+
+for line in xml_lines[i:]:
+    
+    if hello_end in line:
+        nline = line.replace(']]>]]>','',1)
+    else:
+        nline = line
+    
+    # print "LINE: ", nline
+    clean_xml.append(nline)
+
+
+
+xml_data = '\n'.join(clean_xml)
+# print xml_data
+dom = parseString(xml_data)
+xml = dom.toprettyxml()
+# print(xml)
+
+ns = {'mod': 'http://www.cisco.com/nxos:1.0:if_manager',
+    'nxos': 'http://www.cisco.com/nxos:1.0'}
+
+root = ET.fromstring(xml_data)
+ET.dump(root)
+# root = tree.getroot()
+# ET.dump(root)
+# print root.findall(".//mod:interface", namespaces=ns)
+
+for interface in root.findall('.//mod:ROW_interface', namespaces=ns):
+    vlan = interface.find('.//mod:vlan', ns)
+    speed = interface.find('.//mod:speed', ns)
+    int = interface.find('.//mod:interface', ns)
+#print "Interface: ",int.text
+#print " VLAN: ",vlan.text, "Speed: ", speed.text
+
+for interface in root.findall('.//mod:ROW_interface', namespaces=ns):
+    for int_config in interface.iter():
+        short_int = int_config.tag.split('}', 1)
+        # int_config.tag = int_config.tag[len(ns['mod'])]
+        print short_int[1],"\t ",int_config.text
+
+
 exit(0)
 
-for child in root:
-	print(child.tag, child.attrib)
-
-
-exit(0)
